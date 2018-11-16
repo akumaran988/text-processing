6e59d68efde92ff99a9e83b57a114dce06a71004
Ashwin Kumaran K R <36545742+akumaran988@users.noreply.github.com>
2018-11-16 07:22:55 +0530

    Update basic_text_processing.py

--- basic_text_processing.py
+++ basic_text_processing.py
@@ -1,8 +1,12 @@
+#%%
 import numpy as np
 import re
-from sklearn.svm import SVC
-
-
+from keras.models import Sequential as Sequential
+from keras.layers import Dense
+#%%
+import sys
+print(sys.path)
+#%%
 # Reading from a txt file
 f = open('1.txt', 'r')
 lines = f.readlines()
@@ -12,7 +16,6 @@ for line in lines:
     if line.strip():
         print('1' + line.lower())
         striped_lines.append(line.lower())
-
 
 # Creating a dict with some values (important values)
 local_dict = {'trade party': 0, 'isin': 1, 'value date': 2, 'trade date': 3}
@@ -58,27 +61,44 @@ print("Train output vector shape: " + str(train_output_vector.shape))
 
 print('\nLocal dict\n')
 for key in local_dict.keys():
-    train_input_vector[local_dict[key], local_dict[key]] = 1
-    if key == 'isin':
-        train_output_vector[local_dict[key]] = 1
+        train_input_vector[local_dict[key], local_dict[key]] = 1
+        if key == 'isin':
+                train_output_vector[local_dict[key]] = 1
+        if key == 'trade party':
+                train_output_vector[local_dict[key]] = 2
+        if key == 'value date':
+                train_output_vector[local_dict[key]] = 3
+        if key == 'trade date':
+                train_output_vector[local_dict[key]] = 4
+        
 
 print("Train input vector: " + str(train_input_vector))
 print("Train output vector: " + str(train_output_vector))
 
-clf = SVC(kernel='poly', degree=20)
-clf.fit(train_input_vector, train_output_vector)
 
-print('\n\n')
-print('print' + str(clf.predict(train_input_vector)))
+# Converting the output label for multi class
+inputs = train_output_vector
+print(inputs.shape[0])
+max_value = inputs.max(0)
+print(max_value)
+output = np.zeros((inputs.shape[0], int(max_value)+1), int)
+print(output)
+for index, inp in enumerate(inputs):
+        print(str(index) + ' ' + str(inp))
+        output[index, int(inp)] = 1
 
-from keras.models import Sequential
-from keras.layers import Dense
+print(output)
 
-# create model
+train_output_vector = output
+
+print(train_input_vector.shape)
+print(train_output_vector.shape)
+# Model Creation
+# create a sequential model
 model = Sequential()
-model.add(Dense(50, input_dim=31, init='uniform', activation='relu'))
+model.add(Dense(500, input_dim=31, init='uniform', activation='relu'))
 model.add(Dense(50, init='uniform', activation='relu'))
-model.add(Dense(1, init='uniform', activation='sigmoid'))
+model.add(Dense(5, activation='sigmoid'))
 
 # Compile model
 model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
@@ -86,12 +106,17 @@ model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
 model.fit(train_input_vector, train_output_vector, epochs=100, batch_size=10,  verbose=2)
 # calculate predictions
 predictions = model.predict(train_input_vector)
+
 # round predictions
-rounded = [round(x[0]) for x in predictions]
-print(rounded)
+print(predictions)
+for i in range(predictions.shape[0]):
+        for j in range(predictions.shape[1]):
+                predictions[i][j] = round(predictions[i][j])
 
+print(predictions)
+#%%
 # Testing
-
+#%%
 ##
 f = open('1.txt', 'r')
 lines = f.readlines()
